2cf131823117ba9037bbb30bf73dbf5cdcd21a12
joaquingalang <galangjoaquin@gmail.com>
2025-02-08 16:36:54 +0800

    created edit_profile_picture_button.dart

---
+++ lib/widgets/buttons/edit_profile_picture_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:diction_dash/utils/constants.dart';
+
+class EditProfilePictureButton extends StatelessWidget {
+  const EditProfilePictureButton({
+    super.key,
+    this.imageUrl,
+    required this.onTap,
+  });
+
+  final String? imageUrl;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: CircleAvatar(
+        backgroundImage: (imageUrl != null) ? NetworkImage(imageUrl!) : AssetImage('assets/images/placeholder_profile.png'),
+        radius: 70,
+        child: Align(
+          alignment: Alignment.bottomRight,
+          child: Container(
+            width: 35,
+            height: 35,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: kGrayColor600,
+            ),
+            child: const Icon(Icons.edit,
+                color: Colors.white, size: 25),
+          ),
+        ),
+      ),
+    );
+  }
+}
