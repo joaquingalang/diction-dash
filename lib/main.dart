19bb55693e79d35f2d4a32326e6027d1d72e0a69
joaquingalang <galangjoaquin@gmail.com>
2025-02-11 23:36:50 +0800

    added firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:diction_dash/firebase_options.dart';
 import 'package:diction_dash/screens/authentication/landing_screen.dart';
 import 'package:diction_dash/screens/authentication/login_screen.dart';
 import 'package:diction_dash/screens/authentication/register_screen.dart';
@@ -16,7 +18,9 @@ import 'package:diction_dash/screens/settings/settings_screen.dart';
 import 'package:diction_dash/screens/settings/profile_screen.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     const DictionDash(),
   );
