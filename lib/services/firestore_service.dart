3f79b4ed47c755a2cc09dfaf40b41650c50734bc
joaquingalang <galangjoaquin@gmail.com>
2025-02-18 01:51:53 +0800

    added get user data method

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,11 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:diction_dash/models/user_model.dart';
 
 class FirestoreService {
-
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
+  // Get User Data
+  Future<UserModel> getUserData({required String userID}) async {
+    // Get Users Collection Reference & New User Document Reference
+    CollectionReference users = _firestore.collection('users');
+    DocumentReference user = users.doc(userID);
+
+    // Get User Data
+    late Map<String, dynamic> userData;
+    await user.get().then(
+      (DocumentSnapshot doc) {
+        userData = doc.data() as Map<String, dynamic>;
+      },
+      onError: (e) => print('Get User Data Error: $e'),
+    );
+
+    // Store User Data in User Model
+    UserModel userModel = UserModel(
+      userID: userID,
+      username: userData['username'],
+      email: userData['email'],
+    );
+
+    // Return User Model
+    return userModel;
+  }
+
   // Add New User (Username & Email)
-  void addNewUser({required String userID, required String username, required String email}) {
+  void addNewUser(
+      {required String userID,
+      required String username,
+      required String email}) async {
     // Get Users Collection Reference & New User Document Reference
     CollectionReference users = _firestore.collection('users');
     DocumentReference newUser = users.doc(userID);
@@ -20,12 +49,43 @@ class FirestoreService {
       'exp': 0,
       'maxExp': 0, // TODO: Change maxExp to something more appropriate
     };
-    newUser.set(userData);
-    // TODO: Add Minigame Level, EXP, and MaxEXP
+    await newUser.set(userData);
+
+    // Initializes spelling subcollection to store game data
+    final spelling = newUser.collection('spelling').doc('spelling_data');
+    await spelling.set({
+      'level': 1,
+      'exp': 0,
+      'previous_max_exp': 0,
+    });
+
+    // Initializes vocabulary subcollection to store game data
+    final vocabulary = newUser.collection('vocabulary').doc('vocabulary_data');
+    await vocabulary.set({
+      'level': 1,
+      'exp': 0,
+      'previous_max_exp': 0,
+    });
+
+    // Initializes grammar subcollection to store game data
+    final grammar = newUser.collection('grammar').doc('grammar_data');
+    await grammar.set({
+      'level': 1,
+      'exp': 0,
+      'previous_max_exp': 0,
+    });
+
+    // Initializes comprehension subcollection to store game data
+    final comprehension =
+        newUser.collection('comprehension').doc('comprehension_data');
+    await comprehension.set({
+      'level': 1,
+      'exp': 0,
+      'previous_max_exp': 0,
+    });
   }
 
-  // Update Username
+// Update Username
 
-  // Update Profile Picture
-
+// Update Profile Picture
 }
