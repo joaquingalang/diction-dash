8e2aa19f2cbfd0c5e193e836ff57c80fb1f7afd5
joaquingalang <galangjoaquin@gmail.com>
2025-02-18 01:51:22 +0800

    added user_model.dart

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+class UserModel {
+  const UserModel({
+    required this.userID,
+    required this.username,
+    required this.email,
+  });
+
+  final String userID;
+  final String username;
+  final String email;
+}
