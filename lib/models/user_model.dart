97fba7755b2e12789fb7353a19bb41bf1f7b29df
joaquingalang <galangjoaquin@gmail.com>
2025-02-18 12:18:12 +0800

    added provider dependency

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,9 +3,19 @@ class UserModel {
     required this.userID,
     required this.username,
     required this.email,
+    this.fluency,
+    this.profilePicture,
+    this.level = 1,
+    this.exp = 0,
+    this.maxExp = 100, // TODO: Update this to a more appropriate value
   });
 
   final String userID;
   final String username;
   final String email;
+  final String? fluency;
+  final String? profilePicture;
+  final int level;
+  final int exp;
+  final int maxExp;
 }
