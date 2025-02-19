73a4e559577e2181c71cfb25642591988ad7ea66
joaquingalang <galangjoaquin@gmail.com>
2025-02-19 22:13:39 +0800

    added multiprovider to pass user information to home screen

--- lib/screens/authentication/auth_manager.dart
+++ lib/screens/authentication/auth_manager.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:diction_dash/models/user_model.dart';
+import 'package:diction_dash/models/minigame_stats.dart';
 import 'package:diction_dash/services/auth_service.dart';
+import 'package:diction_dash/services/firestore_service.dart';
 import 'package:diction_dash/screens/authentication/landing_screen.dart';
 import 'package:diction_dash/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 // TODO: Since AuthManager returns the landing screen and not the registration or login screen, once a user logs in it does not automatically navigate to home screen
 // TODO: ^ Temporary fix is to navigate back to AuthManager after sign up or sign in
@@ -15,17 +19,51 @@ class AuthManager extends StatefulWidget {
 }
 
 class _AuthManagerState extends State<AuthManager> {
+  // Firebase Authentication Instance
   final AuthService _auth = AuthService();
+
+  // Cloud Firestore Instance
+  final FirestoreService _firestore = FirestoreService();
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: _auth.authListener(),
+      stream: _auth.authListener,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          return HomeScreen();
+          String userID = _auth.currentUserID;
 
-          // TODO: If snapshot has data but the user does not have a fluency score yet, return the FluencyScreen instead.
+          // TODO: Implement StatModel StreamProvider for each Stat (Make inherited stats)
+          return MultiProvider(
+            providers: [
+              StreamProvider<UserModel>(
+                create: (context) => _firestore.getUserData(userID: userID),
+                initialData:
+                    UserModel(username: 'Loading...', email: 'Loading...'),
+              ),
+              StreamProvider<SpellingStats>(
+                create: (context) => _firestore.getSpellingData(userID: userID),
+                initialData:
+                SpellingStats(),
+              ),
+              StreamProvider<VocabularyStats>(
+                create: (context) => _firestore.getVocabularyData(userID: userID),
+                initialData:
+                VocabularyStats(),
+              ),
+              StreamProvider<GrammarStats>(
+                create: (context) => _firestore.getGrammarData(userID: userID),
+                initialData:
+                GrammarStats(),
+              ),
+              StreamProvider<ComprehensionStats>(
+                create: (context) => _firestore.getComprehensionData(userID: userID),
+                initialData:
+                ComprehensionStats(),
+              ),
+            ],
+            child: HomeScreen(),
+          );
 
         } else {
           return LandingScreen();
