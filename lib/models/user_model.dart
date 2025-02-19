0f522255147c96a87b77cdd8f7314a0709f7d0aa
joaquingalang <galangjoaquin@gmail.com>
2025-02-19 22:12:35 +0800

    updated user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserModel {
+  // Constructor
   const UserModel({
-    required this.userID,
     required this.username,
     required this.email,
     this.fluency,
@@ -10,12 +12,25 @@ class UserModel {
     this.maxExp = 100, // TODO: Update this to a more appropriate value
   });
 
-  final String userID;
-  final String username;
-  final String email;
+  // Attributes
+  final String? username;
+  final String? email;
   final String? fluency;
   final String? profilePicture;
   final int level;
   final int exp;
   final int maxExp;
+
+  factory UserModel.fromFirestore(DocumentSnapshot snapshot) {
+    Map<String, dynamic> userData = snapshot.data() as Map<String, dynamic>;
+    return UserModel(
+      username: userData['username'] ?? 'Unknown',
+      email: userData['email'] ?? 'Unknown',
+      fluency: userData['fluency'],
+      profilePicture: userData['profile_picture'],
+      level: userData['level'],
+      exp: userData['exp'],
+      maxExp: userData['max_exp'],
+    );
+  }
 }
