df1083cbf3f2deb4f2029a4ffa2cd7582f03a17c
joaquingalang <galangjoaquin@gmail.com>
2025-02-20 23:22:03 +0800

    added firebase app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:diction_dash/firebase_options.dart';
+import 'firebase_options.dart';
 import 'package:diction_dash/screens/authentication/auth_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    name: 'diction-dash',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const DictionDash(),
   );
