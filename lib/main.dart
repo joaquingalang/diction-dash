b974274a0f2bd0a8bf89fbf0956ff2e1cc228ba1
joaquingalang <galangjoaquin@gmail.com>
2025-02-20 17:29:59 +0800

    removed unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:diction_dash/firebase_options.dart';
 import 'package:diction_dash/screens/authentication/auth_manager.dart';
-import 'package:diction_dash/screens/authentication/landing_screen.dart';
-import 'package:diction_dash/screens/authentication/login_screen.dart';
-import 'package:diction_dash/screens/authentication/register_screen.dart';
-import 'package:diction_dash/screens/fluency/fluency_screen.dart';
-import 'package:diction_dash/screens/fluency/feedback_screen.dart';
-import 'package:diction_dash/screens/home_screen.dart';
-import 'package:diction_dash/screens/test_screen.dart';
-import 'package:diction_dash/screens/minigame/spelling_screen.dart';
-import 'package:diction_dash/screens/minigame/vocabulary_screen.dart';
-import 'package:diction_dash/screens/minigame/grammar_screen.dart';
-import 'package:diction_dash/screens/minigame/comprehension_screen.dart';
-import 'package:diction_dash/screens/minigame/end_game_screen.dart';
-import 'package:diction_dash/screens/settings/settings_screen.dart';
-import 'package:diction_dash/screens/settings/profile_screen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
