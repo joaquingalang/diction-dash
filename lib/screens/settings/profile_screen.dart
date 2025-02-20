bbbcc3a4697965fc248fef2baf9a105212e16259
joaquingalang <galangjoaquin@gmail.com>
2025-02-20 17:30:54 +0800

    added update username functionality

--- lib/screens/settings/profile_screen.dart
+++ lib/screens/settings/profile_screen.dart
@@ -1,6 +1,10 @@
+import 'package:diction_dash/widgets/loading_indicators/fox_loading_indicator.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:diction_dash/utils/constants.dart';
+import 'package:diction_dash/models/user_model.dart';
 import 'package:diction_dash/services/auth_service.dart';
+import 'package:diction_dash/services/firestore_service.dart';
 import 'package:diction_dash/screens/fluency/fluency_screen.dart';
 import 'package:diction_dash/screens/authentication/auth_manager.dart';
 import 'package:diction_dash/widgets/buttons/profile_picture_edit_button.dart';
@@ -18,16 +22,21 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+  // Firebase Authentication Instance
   final AuthService _auth = AuthService();
 
-  void _updateUsername(BuildContext context) {
+  // Cloud Firestore Instance
+  final FirestoreService _firestore = FirestoreService();
+
+  void _updateUsername(BuildContext context, {required String username}) {
     showModalBottomSheet(
       context: context,
 
       // Added responsive bottom padding for when keyboard is toggled
       builder: (context) => Padding(
         padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-        child: UpdateUsernameSheet(),
+        child: UpdateUsernameSheet(username: username),
       ),
       isScrollControlled: true,
     );
@@ -95,60 +104,71 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
 
       // Page Body
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              // Edit Profile Picture
-              ProfilePictureEditButton(onTap: () {}),
+      body: StreamBuilder<UserModel>(
+        stream: _firestore.getUserData(userID: _auth.currentUserID),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            UserModel userData = snapshot.data as UserModel;
+            String username = userData.username!;
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    // Edit Profile Picture
+                    ProfilePictureEditButton(onTap: () {}),
 
-              // Edit Username
-              ProfileEditButton(
-                  text: 'USERNAME', labelText: 'Alice Guo', onTap: () => _updateUsername(context)),
+                    // Edit Username
+                    ProfileEditButton(
+                        text: 'USERNAME', labelText: username, onTap: () => _updateUsername(context, username: username)),
 
-              // Change Password
-              ProfileEditButton(
-                  text: 'PASSWORD', labelText: '**********', onTap: () => _updatePassword(context)),
+                    // Change Password
+                    ProfileEditButton(
+                        text: 'PASSWORD', labelText: '**********', onTap: () => _updatePassword(context)),
 
-              // Adjust Fluency
-              RoundedRectangleButton(
-                backgroundColor: kGrayColor300,
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FluencyScreen(),
-                  ),
-                ),
-                child: Center(
-                  child: Text('ADJUST FLUENCY', style: kButtonTextStyle),
+                    // Adjust Fluency
+                    RoundedRectangleButton(
+                      backgroundColor: kGrayColor300,
+                      onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => FluencyScreen(),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text('ADJUST FLUENCY', style: kButtonTextStyle),
+                      ),
+                    ),
+
+                    // Delete Account
+                    RoundedRectangleButton(
+                      backgroundColor: kGrayColor300,
+                      onPressed: () => _deleteAccount(context),
+                      child: Center(
+                        child: Text('DELETE ACCOUNT', style: kButtonTextStyle),
+                      ),
+                    ),
+
+                    // Expanded Offset
+                    Expanded(
+                      child: SizedBox(height: 30),
+                    ),
+
+                    // Logout Button
+                    RoundedRectangleButton(
+                      onPressed: _logout,
+                      child: Center(
+                        child: Text('LOGOUT', style: kButtonTextStyle),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-
-              // Delete Account
-              RoundedRectangleButton(
-                backgroundColor: kGrayColor300,
-                onPressed: () => _deleteAccount(context),
-                child: Center(
-                  child: Text('DELETE ACCOUNT', style: kButtonTextStyle),
-                ),
-              ),
-
-              // Expanded Offset
-              Expanded(
-                child: SizedBox(height: 30),
-              ),
-
-              // Logout Button
-              RoundedRectangleButton(
-                onPressed: _logout,
-                child: Center(
-                  child: Text('LOGOUT', style: kButtonTextStyle),
-                ),
-              ),
-            ],
-          ),
-        ),
+            );
+          } else {
+            return Foxloadingindicator();
+          }
+        }
       ),
     );
   }
