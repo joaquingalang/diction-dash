f00ff6581a289904d93f33f215e6fa121a100e76
joaquingalang <galangjoaquin@gmail.com>
2025-02-28 02:18:31 +0800

    updated end game screen to be dynamic

--- lib/screens/minigame/end_game_screen.dart
+++ lib/screens/minigame/end_game_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:diction_dash/utils/constants.dart';
 import 'package:confetti/confetti.dart';
+import 'package:diction_dash/services/game_audio.dart';
 import 'package:diction_dash/screens/authentication/auth_manager.dart';
 import 'package:diction_dash/widgets/progress_bars/question_bar.dart';
 import 'package:diction_dash/widgets/progress_bars/score_bar.dart';
@@ -19,22 +20,70 @@ class EndGameScreen extends StatefulWidget {
 }
 
 class _EndGameScreenState extends State<EndGameScreen> {
+
+  // Game Audio
+  final GameAudio _gameAudio = GameAudio();
+
+  // Confetti Controller
   final ConfettiController _confettiController = ConfettiController();
 
+  // Performance & Star Colors
+  String performance = 'GOOD EFFORT!';
+  List<Color> starColors = [Colors.grey, Colors.grey, Colors.grey];
+
   void _playConfetti() async {
-    // Play Confetti
-    _confettiController.play();
 
-    // 10 second delay to ensure that the animation played (for low-performance devices)
-    await Future.delayed(Duration(seconds: 10));
+    if (widget.score >= 5) {
+      // Play Confetti
+      _confettiController.play();
 
-    // Stop confetti controller
-    _confettiController.stop();
+      // 10 second delay to ensure that the animation played (for low-performance devices)
+      await Future.delayed(Duration(seconds: 10));
+
+      // Stop confetti controller
+      _confettiController.stop();
+    }
+  }
+
+  Future<void> _playJingle() async {
+    if (widget.score > 0) {
+      _gameAudio.congratulations();
+    } else {
+      _gameAudio.flop();
+    }
+  }
+
+  void _setPerformance() {
+    if (widget.score == 0) {
+      performance = 'TRY AGAIN!';
+    } else if (widget.score <= 4) {
+      performance = 'GOOD EFFORT!';
+    } else if (widget.score <= 8) {
+      performance = 'WELL DONE!';
+    } else {
+      performance = 'EXCELLENT!';
+    }
+  }
+
+  void _setStarColors() {
+    // Set star colors based on score
+    if (widget.score == 0) {
+      starColors = [Colors.grey, Colors.grey, Colors.grey];
+    } else if (widget.score <= 4) {
+      starColors = [Colors.yellow, Colors.grey, Colors.grey];
+    } else if (widget.score <= 8) {
+      starColors = [Colors.yellow, Colors.yellow, Colors.grey];
+    } else {
+      starColors = [Colors.yellow, Colors.yellow, Colors.yellow];
+    }
   }
 
   @override
   void initState() {
     super.initState();
+    _setPerformance();
+    _playJingle();
+    _setStarColors();
     _playConfetti();
   }
 
@@ -86,7 +135,7 @@ class _EndGameScreenState extends State<EndGameScreen> {
                 SizedBox(height: 30),
 
                 // Performance Evaluation
-                Text('EXCELLENT!', style: kOswaldXLarge),
+                Text(performance!, style: kOswaldXLarge),
 
                 // Performance & Rewards
                 Column(
@@ -101,7 +150,7 @@ class _EndGameScreenState extends State<EndGameScreen> {
                     // Experience Points
                     Text('EXPERIENCE POINTS', style: kOswaldMedium),
                     Text(
-                      '+ 1028 XP',
+                      '+ ${widget.score * 10} XP',
                       style: kOrangeButtonTextStyle.copyWith(fontSize: 36),
                     ),
                   ],
@@ -111,9 +160,9 @@ class _EndGameScreenState extends State<EndGameScreen> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.star, size: 120, color: Colors.yellow),
-                    Icon(Icons.star, size: 120, color: Colors.yellow),
-                    Icon(Icons.star, size: 120, color: Colors.grey),
+                    Icon(Icons.star, size: 120, color: starColors![0]),
+                    Icon(Icons.star, size: 120, color: starColors![1]),
+                    Icon(Icons.star, size: 120, color: starColors![2]),
                   ],
                 ),
 
