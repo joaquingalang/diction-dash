1d0835ac7de5069628aebfe665932ebb34c865c7
joaquingalang <galangjoaquin@gmail.com>
2025-02-28 02:18:09 +0800

    finished quesiton bank methods

--- lib/services/question_bank.dart
+++ lib/services/question_bank.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'dart:io';
 
 class QuestionBank {
-
   const QuestionBank({required this.fluency});
 
   final String fluency;
@@ -40,30 +39,25 @@ class QuestionBank {
   }
 
   // Fetch 10 Spelling Questions Based on Fluency
-
-  // Fetch 10 Vocabulary Questions Based on Fluency
-
-  // Fetch 10 Grammar Questions Based on Fluency
-  Future<List<Map<String, dynamic>>> getGrammarQuestions(int questions) async {
-
-    // Get Grammar Question
-    String jsonString = await rootBundle.loadString('assets/questions/grammar_bank.json');
-    Map<String, dynamic> grammarQuestions = jsonDecode(jsonString) as Map<String, dynamic>;
+  Future<List<Map<String, dynamic>>> getSpellingQuestions(int questions) async {
+    // Get spelling question from JSON file
+    String jsonString =
+        await rootBundle.loadString('assets/questions/spelling_bank.json');
+    Map<String, dynamic> spellingQuestions =
+        jsonDecode(jsonString) as Map<String, dynamic>;
 
     // Get ratio based on fluency
-    // Define Map<String, int> with difficulty level as key and number of questions as values
     Map<String, double> difficultyRatio = getDifficultyRatio();
 
-    // For each difficulty level generate number of questions and append it to List
-    // Ensure the questions are not the same
+    // For each difficulty level generate number of questions and append it to a list
     List<Map<String, dynamic>> questionList = [];
-
     for (String difficulty in difficultyRatio.keys) {
-      List difficultyQuestions = grammarQuestions[difficulty];
+      List difficultyQuestions = spellingQuestions[difficulty];
       double ratio = difficultyRatio[difficulty]!;
       int numOfQuestions = (questions * ratio).toInt();
       List<int> questionIndices = [];
 
+      // Generate 10 unique integers
       while (questionIndices.length < numOfQuestions) {
         Random random = Random();
         int maxRange = difficultyQuestions.length;
@@ -73,19 +67,179 @@ class QuestionBank {
         }
       }
 
+      // Generate corresponding questions from unique integers
       for (int questionIndex in questionIndices) {
         Map<String, dynamic> newQuestion = difficultyQuestions[questionIndex];
         questionList.add(newQuestion);
       }
     }
 
-    // Shuffle list
+    // Shuffle question list
     questionList.shuffle();
 
-    // Return list
+    // Return question list
+    return questionList;
+  }
+
+  // Fetch 10 Vocabulary Questions Based on Fluency
+  Future<List<Map<String, dynamic>>> getVocabularyQuestions(
+      int questions) async {
+    // Get spelling question from JSON file
+    String jsonString =
+        await rootBundle.loadString('assets/questions/vocabulary_bank.json');
+    Map<String, dynamic> vocabularyQuestions = jsonDecode(jsonString) as Map<String, dynamic>;
+
+    // Get ratio based on fluency
+    Map<String, double> difficultyRatio = getDifficultyRatio();
+    Random random = Random();
+
+    // For each difficulty level generate number of questions and append it to a list
+    List<Map<String, dynamic>> questionList = [];
+    for (String difficulty in difficultyRatio.keys) {
+      List difficultyQuestions = vocabularyQuestions[difficulty];
+      double ratio = difficultyRatio[difficulty]!;
+      int numOfQuestions = (questions * ratio).toInt();
+      List<int> questionIndices = [];
+
+      // Generate 10 unique integers
+      while (questionIndices.length < numOfQuestions) {
+        int maxRange = difficultyQuestions.length;
+        int questionIndex = random.nextInt(maxRange);
+        if (!questionIndices.contains(questionIndex)) {
+          questionIndices.add(questionIndex);
+        }
+      }
+
+      String jsonString =
+      await rootBundle.loadString('assets/questions/word_choices.json');
+      Map<String, dynamic> wordChoices = jsonDecode(jsonString) as Map<String, dynamic>;
+      List words = wordChoices['words'];
+
+      // Generate corresponding questions from unique integers
+      for (int questionIndex in questionIndices) {
+
+        // Read vocabulary question data
+        Map<String, dynamic> question = difficultyQuestions[questionIndex];
+
+        // Choose a synonym as the correct answer
+        List synonyms = question['synonyms'];
+        String synonym = synonyms[random.nextInt(synonyms.length)];
+
+        // Generate three unique random words as choices
+        List<String> choices = [];
+        while (choices.length < 3) {
+          int maxRange = words.length;
+          int wordIndex = random.nextInt(maxRange);
+          String word = words[wordIndex];
+          if (!choices.contains(word)) {
+            choices.add(word);
+          }
+        }
+
+        // Add the correct answer to choices and shuffle
+        choices.add(synonym);
+        choices.shuffle();
+
+        // Initialize the new question with all of its components
+        Map<String, dynamic> newQuestion = {
+          'word': question['word'],
+          'choices': choices,
+          'answer': synonym,
+        };
+        questionList.add(newQuestion);
+      }
+    }
+
+    // Shuffle question list
+    questionList.shuffle();
+
+    // Return question list
+    return questionList;
+  }
+
+  // Fetch 10 Grammar Questions Based on Fluency
+  Future<List<Map<String, dynamic>>> getGrammarQuestions(int questions) async {
+    // Get grammar questions from JSON file
+    String jsonString =
+        await rootBundle.loadString('assets/questions/grammar_bank.json');
+    Map<String, dynamic> grammarQuestions =
+        jsonDecode(jsonString) as Map<String, dynamic>;
+
+    // Get ratio based on fluency
+    Map<String, double> difficultyRatio = getDifficultyRatio();
+
+    // For each difficulty level generate number of questions and append it to a list
+    List<Map<String, dynamic>> questionList = [];
+    for (String difficulty in difficultyRatio.keys) {
+      List difficultyQuestions = grammarQuestions[difficulty];
+      double ratio = difficultyRatio[difficulty]!;
+      int numOfQuestions = (questions * ratio).toInt();
+      List<int> questionIndices = [];
+
+      // Generate 10 unique integers
+      while (questionIndices.length < numOfQuestions) {
+        Random random = Random();
+        int maxRange = difficultyQuestions.length;
+        int questionIndex = random.nextInt(maxRange);
+        if (!questionIndices.contains(questionIndex)) {
+          questionIndices.add(questionIndex);
+        }
+      }
+
+      // Generate corresponding questions from unique integers
+      for (int questionIndex in questionIndices) {
+        Map<String, dynamic> newQuestion = difficultyQuestions[questionIndex];
+        questionList.add(newQuestion);
+      }
+    }
+
+    // Shuffle question list
+    questionList.shuffle();
+
+    // Return question list
     return questionList;
   }
 
   // Fetch 10 Comprehension Questions Based on Fluency
+  Future<List<Map<String, dynamic>>> getComprehensionQuestions(int questions) async {
+    // Get comprehension questions from JSON file
+    String jsonString =
+    await rootBundle.loadString('assets/questions/comprehension_bank.json');
+    Map<String, dynamic> comprehensionQuestions = jsonDecode(jsonString) as Map<String, dynamic>;
+
+    // Get ratio based on fluency
+    Map<String, double> difficultyRatio = getDifficultyRatio();
+
+    // For each difficulty level generate number of questions and append it to a list
+    List<Map<String, dynamic>> questionList = [];
+    for (String difficulty in difficultyRatio.keys) {
+      List difficultyQuestions = comprehensionQuestions[difficulty];
+      double ratio = difficultyRatio[difficulty]!;
+      int numOfQuestions = (questions * ratio).toInt();
+      List<int> questionIndices = [];
+
+      // Generate 10 unique integers
+      while (questionIndices.length < numOfQuestions) {
+        Random random = Random();
+        int maxRange = difficultyQuestions.length;
+        int questionIndex = random.nextInt(maxRange);
+        if (!questionIndices.contains(questionIndex)) {
+          questionIndices.add(questionIndex);
+        }
+      }
+
+      // Generate corresponding questions from unique integers
+      for (int questionIndex in questionIndices) {
+        Map<String, dynamic> newQuestion = difficultyQuestions[questionIndex];
+        questionList.add(newQuestion);
+      }
+    }
+
+    // Shuffle question list
+    questionList.shuffle();
+
+    // Return question list
+    return questionList;
+  }
 
 }
