d2f7f78507683ff5cc20d7dbdc949de847686074
joaquingalang <galangjoaquin@gmail.com>
2025-03-07 02:36:30 +0800

    integrated settings service to settings screen

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,5 +1,7 @@
+import 'package:diction_dash/widgets/loading_indicators/fox_loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:diction_dash/utils/constants.dart';
+import 'package:diction_dash/services/settings_service.dart';
 import 'package:diction_dash/screens/settings/profile_screen.dart';
 import 'package:diction_dash/widgets/list_tiles/setting_tile.dart';
 import 'package:diction_dash/widgets/list_tiles/switch_setting_tile.dart';
@@ -14,94 +16,115 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  // Settings Instances
+  final SettingsService _settings = SettingsService();
+
+  // Page Loading
+  bool _isLoading = true;
 
   // Toggle Variables
   bool _notifsEnabled = true;
   bool _capslockEnabled = true;
-  bool _soundsEnabled = true;
+  bool _gameAudioEnabled = true;
+
+  void _initSettings() async {
+    bool? gameAudio = await _settings.getGameAudio();
+    setState(() {
+      _gameAudioEnabled = gameAudio;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initSettings();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-      // Page App Bar
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(
-            Icons.arrow_back,
-            size: 35,
-          ),
-        ),
-        title: Text('Settings', style: kOswaldMedium),
-      ),
-
-      // Page Body
-      body: SafeArea(
-        child: Column(
-          children: [
-            SettingTile(
-              title: 'Profile',
-              iconData: Icons.person,
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProfileScreen(),
+    return (!_isLoading)
+        ? Scaffold(
+            // Page App Bar
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              centerTitle: true,
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back,
+                  size: 35,
                 ),
               ),
+              title: Text('Settings', style: kOswaldMedium),
             ),
-            SettingTile(
-              title: 'Preferences',
-              iconData: Icons.photo,
-              onTap: () {},
+
+            // Page Body
+            body: SafeArea(
+              child: Column(
+                children: [
+                  SettingTile(
+                    title: 'Profile',
+                    iconData: Icons.person,
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProfileScreen(),
+                      ),
+                    ),
+                  ),
+                  SettingTile(
+                    title: 'Preferences',
+                    iconData: Icons.photo,
+                    onTap: () {},
+                  ),
+                  SwitchSettingTile(
+                    title: 'Notifications',
+                    iconData: Icons.notifications,
+                    value: _notifsEnabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _notifsEnabled = value;
+                      });
+                    },
+                  ),
+                  SwitchSettingTile(
+                    title: 'Auto caps-lock',
+                    iconData: Icons.notifications,
+                    value: _capslockEnabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _capslockEnabled = value;
+                      });
+                    },
+                  ),
+                  SwitchSettingTile(
+                    title: 'Game Audio',
+                    iconData: Icons.volume_up,
+                    value: _gameAudioEnabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _gameAudioEnabled = value;
+                      });
+                      _settings.setGameAudio(_gameAudioEnabled);
+                    },
+                  ),
+                  SettingTile(
+                    title: 'FAQ',
+                    iconData: Icons.help,
+                    onTap: () {},
+                  ),
+                  SettingTile(
+                    title: 'Help & Support',
+                    iconData: Icons.info,
+                    onTap: () {},
+                  ),
+                ],
+              ),
             ),
-            SwitchSettingTile(
-              title: 'Notifications',
-              iconData: Icons.notifications,
-              value: _notifsEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _notifsEnabled = value;
-                });
-              },
-            ),
-            SwitchSettingTile(
-              title: 'Auto caps-lock',
-              iconData: Icons.notifications,
-              value: _capslockEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _capslockEnabled = value;
-                });
-              },
-            ),
-            SwitchSettingTile(
-              title: 'Sounds',
-              iconData: Icons.volume_up,
-              value: _soundsEnabled,
-              onChanged: (value) {
-                setState(() {
-                  _soundsEnabled = value;
-                });
-              },
-            ),
-            SettingTile(
-              title: 'FAQ',
-              iconData: Icons.help,
-              onTap: () {},
-            ),
-            SettingTile(
-              title: 'Help & Support',
-              iconData: Icons.info,
-              onTap: () {},
-            ),
-          ],
-        ),
-      ),
-    );
+          )
+        : Foxloadingindicator();
   }
 }
