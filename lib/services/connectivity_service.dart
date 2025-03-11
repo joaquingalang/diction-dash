8018b0bb63d5ce72e1943f5d84db7e701d6e96d9
joaquingalang <galangjoaquin@gmail.com>
2025-03-11 22:53:46 +0800

    renamed class to ConnectivityService

---
+++ lib/services/connectivity_service.dart
@@ -0,0 +1,7 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+class ConnectivityService {
+
+  final InternetConnectionChecker _connectionChecker = InternetConnectionChecker.instance;
+
+}
